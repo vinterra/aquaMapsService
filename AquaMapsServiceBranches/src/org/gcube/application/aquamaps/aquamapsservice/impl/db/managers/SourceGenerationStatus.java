5c0f94174044bc6cd0994028155b1f307bb871a5
fabio.sinibaldi <fabio.sinibaldi@mycompany.com>
2010-12-03 12:54:15 +0000

    wrong path

--- AquaMapsServiceBranches/src/org/gcube/application/aquamaps/aquamapsservice/impl/db/managers/SourceGenerationStatus.java
+++
@@ -1,5 +0,0 @@
-package org.gcube.application.aquamaps.aquamapsservice.impl.db.managers;
-
-public enum SourceGenerationStatus {
-	Pending,Importing,Merging,Registering,Error,Complete
-}
--- AquaMapsServiceBranches/src/org/gcube/application/aquamaps/aquamapsservice/impl/db/managers/SubmittedStatus.java
+++
@@ -1,5 +0,0 @@
-package org.gcube.application.aquamaps.aquamapsservice.impl.db.managers;
-
-public enum SubmittedStatus {
-	Pending,Simulating,Generating,Publishing,Completed,Error
-}
