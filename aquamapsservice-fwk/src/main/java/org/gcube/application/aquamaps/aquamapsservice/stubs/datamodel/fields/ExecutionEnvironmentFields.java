e984d547f89f0c78cb22ecb07d9b91c3fad25036
fabio.sinibaldi <fabio.sinibaldi@mycompany.com>
2013-01-10 11:24:18 +0000

    moved to CL

--- aquamapsservice-fwk/src/main/java/org/gcube/application/aquamaps/aquamapsservice/stubs/datamodel/fields/ExecutionEnvironmentFields.java
+++
@@ -1,8 +0,0 @@
-package org.gcube.application.aquamaps.aquamapsservice.stubs.datamodel.fields;
-
-public enum ExecutionEnvironmentFields {
-
-	environment,resource_number,max_resource,backend,endpoint_url,logo,description, policies, disclaimer,cloud
-	
-	
-}
--- aquamapsservice-fwk/src/main/java/org/gcube/application/aquamaps/aquamapsservice/stubs/datamodel/types/AlgorithmType.java
+++
@@ -1,8 +0,0 @@
-package org.gcube.application.aquamaps.aquamapsservice.stubs.datamodel.types;
-
-public enum AlgorithmType {
-
-	NativeRange,SuitableRange,NativeRange2050,SuitableRange2050,HSPENRegeneration,
-	LINEAR,PARABOLIC
-	
-}
